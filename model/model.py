faefc0237d600b0122361119274d4d69d72348ee
Conghao Shen <me@tomshen.io>
2023-05-10 21:34:37 -0700

    tweak

--- model/model.py
+++ model/model.py
@@ -97,11 +97,3 @@ class TextDetectionModel(DetectionModel):
     def build_dataset(self) -> MangaDataset:
         transform = self.build_image_processor()
         return MangaDataset(frame_annotations=False, text_annotations=True, preprocess=transform)
-
-
-# from transformers import DetrForObjectDetection
-
-# from constant import DETR_PRETRAINED
-
-# def get_model() -> DetrForObjectDetection:
-#     return DetrForObjectDetection.from_pretrained(DETR_PRETRAINED, num_labels=1, ignore_mismatched_sizes=True) # type: ignore
