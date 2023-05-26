e17ed8a53b57494a9eba4473e245efb4ac85e516
Tom Shen <me@tomshen.io>
2023-05-25 20:39:03 -0700

    api (#7)

--- metrics.py
+++
@@ -1,6 +0,0 @@
-from dataset import MangaDataset
-from manga109utils import BoundingBox
-
-def evaluate_bounding_boxes(expected: list[BoundingBox], pred: list[BoundingBox]) -> dict:
-    # TODO
-    pass
